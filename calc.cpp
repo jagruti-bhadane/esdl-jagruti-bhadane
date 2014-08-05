102b938ea9e2b5c2041bfb6360ebdf37f3c015f7
jagruti-bhadane <jagruti.bhadane@cumminscollege.in>
2014-08-05 08:56:52 +0530

    Update calc.cpp
    i hv added one more function to the program........

--- calc.cpp
+++ calc.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 int main()
 {
+ cout<<"hello......";
  cout<<"this is my first file";
  
 return 0;
 
+ }
+ 
+ void add
+ {
+  int a,b,c;
+  cout<<"enter the no u want to add";
+  cin>>a>>b;
+  c=a+b;
+  cout<<"print c";
+  
  }
